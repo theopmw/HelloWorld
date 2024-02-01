7da9c5c0e34f90210c1a52d0ea993a97f87bed33
theopmw <theowrightdev@gmail.com>
2024-02-01 22:31:45 +0000

    add classes

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
-public class FirstClass {
+public class Hello {
     public static void main(String[] args) {
+        System.out.println("Hello, Theo!");
         System.out.print("Hello World!");
     }
 }
