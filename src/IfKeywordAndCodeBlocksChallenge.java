0352a822065f46d7d0ba2d5d80197cd392aed002
theopmw <theowrightdev@gmail.com>
2024-02-15 15:10:37 +0000

    remove comments

--- src/IfKeywordAndCodeBlocksChallenge.java
+++ src/IfKeywordAndCodeBlocksChallenge.java
@@ -15,17 +15,6 @@ public class IfKeywordAndCodeBlocksChallenge {
             System.out.println("Your final score was: " + finalScore);
         }
 
-//        boolean newGameOver = true;
-//        int newScore = 10000;
-//        int newLevelCompleted = 8;
-//        int newBonus = 200;
-//
-//        int newFinalScore = newScore;
-//
-//        if (newGameOver) {
-//            newFinalScore += (newLevelCompleted * newBonus);
-//            System.out.println("Your final score was: " + newFinalScore);
-//        }
 
         gameOver = true;
         score = 10000;
