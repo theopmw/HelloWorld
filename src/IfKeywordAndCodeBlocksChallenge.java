25bdc1f482b2e5c12b37dd96d78b0c53bda25ec0
theopmw <theowrightdev@gmail.com>
2024-02-26 09:41:24 +0000

    refactor calculateScore method

--- src/IfKeywordAndCodeBlocksChallenge.java
+++ src/IfKeywordAndCodeBlocksChallenge.java
@@ -2,20 +2,32 @@ public class IfKeywordAndCodeBlocksChallenge {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 800, 5, 100);
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-        calculateScore(true, 10000, 8, 200);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("the highScore is: " + highScore);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        System.out.println("The next high score is: " +
+                calculateScore(gameOver, score, levelCompleted, bonus));
 
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         int finalScore = score;
 
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was: " + finalScore);
         }
+
+        return finalScore;
     }
 }
