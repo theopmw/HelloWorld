06e36512a9faa4c24c8d9f7c9e1ae7494ff82921
theopmw <theowrightdev@gmail.com>
2024-02-28 06:35:53 +0000

    add calculateHighScorePosition method

--- src/MethodsChallenge.java
+++ src/MethodsChallenge.java
@@ -2,11 +2,25 @@ public class MethodsChallenge {
 
     public static void main(String[] args) {
 
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Theo", highScorePosition);
 
     }
 
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
         System.out.println(playerName + " got into position "
                 + highScorePosition + " on the high score list");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore >=100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
