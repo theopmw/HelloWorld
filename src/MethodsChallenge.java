3a25fe0923d08a95479496bb8a1624acb6d5275b
theopmw <theowrightdev@gmail.com>
2024-02-28 06:44:16 +0000

    refactor calculateHighScorePosition method

--- src/MethodsChallenge.java
+++ src/MethodsChallenge.java
@@ -5,6 +5,14 @@ public class MethodsChallenge {
         int highScorePosition = calculateHighScorePosition(1500);
         displayHighScorePosition("Theo", highScorePosition);
 
+        highScorePosition = calculateHighScorePosition(999);
+        displayHighScorePosition("Lucy", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Benny", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(25);
+        displayHighScorePosition("Lila", highScorePosition);
     }
 
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
@@ -13,14 +21,26 @@ public class MethodsChallenge {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
+
+//        Solution using multiple return statements
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >=100) {
+//            return 3;
+//        }
+//        return 4;
+
+//      Solution using 1 return statement
+        int position = 4;
         if (playerScore >= 1000) {
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
-            return 2;
-        } else if (playerScore >=100 && playerScore < 500) {
-            return 3;
-        } else {
-            return 4;
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >=100) {
+            position = 3;
+        }
+        return position;
         }
     }
-}
