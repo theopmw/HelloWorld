9baf747689506bbca5e9468e4a7013db5d07607a
theopmw <theowrightdev@gmail.com>
2024-02-28 06:00:18 +0000

    start methods challenge, create displayHighScorePosition method

---
+++ src/MethodsChallenge.java
@@ -0,0 +1,12 @@
+public class MethodsChallenge {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " got into position "
+                + highScorePosition + " on the high score list");
+    }
+}
